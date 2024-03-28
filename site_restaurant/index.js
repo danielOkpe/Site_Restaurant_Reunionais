fcb262457646587739b317e167adac8e31387763
danielOkpe <145658821+danielOkpe@users.noreply.github.com>
2024-03-28 18:45:11 +0100

    scroll modif

--- site_restaurant/index.js
+++ site_restaurant/index.js
@@ -1,14 +1,34 @@
 let btn = document.getElementById("horaire");
-let pos = btn.getBoundingClientRect();
-console.log(pos.x);
+let descoverBtn = document.getElementById("descover_btn");
 
-document.getElementById("descover_btn").addEventListener("click", function () {
-  window.scroll({
-    top: pos.y,
-    left: 0,
-    behavior: "smooth",
+let posHorraire = btn.getBoundingClientRect();
+let posDescBtn = descoverBtn.getBoundingClientRect();
+posy = parseInt(posHorraire.y);
+
+let distance = parseInt(posHorraire.y - posDescBtn.y);
+let d = parseInt(distance / 10);
+let pas = 10;
+
+let index = 0;
+let i = 0;
+
+console.log(posDescBtn);
+console.log(distance);
+
+descoverBtn.addEventListener("click", function () {
+  timout = setInterval(() => {
+    window.scrollBy(0, pas);
+    d = d - 1; //posy = posy - 1;
+  }, 1);
+
+  setInterval(() => {
+    if (d == 0) {
+      clearInterval(timout);
+      d = parseInt(distance / 10);
+    }
   });
-  /* document.getElementById("history").scrollIntoView({
+
+  /*document.getElementById("history").scrollIntoView({
     behavior: "smooth",
     block: "center",
     inline: "nearest",
