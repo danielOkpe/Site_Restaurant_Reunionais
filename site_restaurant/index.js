ab011db3d2652e4b0a61684cf5fe1ebe542551da
danielOkpe <145658821+danielOkpe@users.noreply.github.com>
2024-03-29 09:02:53 +0100

    ajustement de la page

--- site_restaurant/index.js
+++ site_restaurant/index.js
@@ -1,16 +1,16 @@
-function scrollToHistory() {
-  var historyElement = document.getElementById("history");
-  var offsetTop = historyElement.offsetTop;
-  var duration = 800; // Durée de l'animation en millisecondes
-  var start = window.pageYOffset;
-  var startTime =
+/*function scrollToHistory() {
+  let historyElement = document.getElementById("history");
+  let offsetTop = historyElement.offsetTop;
+  let duration = 800; // Durée de l'animation en millisecondes
+  let start = window.pageYOffset;
+  let startTime =
     "now" in window.performance ? performance.now() : new Date().getTime();
 
   function scroll() {
-    var now =
+    let now =
       "now" in window.performance ? performance.now() : new Date().getTime();
-    var time = Math.min(1, (now - startTime) / duration);
-    var timeFunction = easeInOutQuad(time);
+    let time = Math.min(1, (now - startTime) / duration);
+    let timeFunction = easeInOutQuad(time);
     window.scroll(0, Math.ceil(timeFunction * (offsetTop - start) + start));
 
     if (Math.abs(window.pageYOffset - offsetTop) < 1 || time >= 1) {
@@ -26,22 +26,22 @@ function scrollToHistory() {
 
   scroll();
 }
+*/
 
-/*
 function scrollToHistory() {
-  var historyElement = document.getElementById("history");
-  var offsetBottom =
+  let historyElement = document.getElementById("history");
+  let offsetBottom =
     historyElement.getBoundingClientRect().top + window.pageYOffset;
-  var duration = 800; // Durée de l'animation en millisecondes
-  var start = window.pageYOffset;
-  var startTime =
+  let duration = 800; // Durée de l'animation en millisecondes
+  let start = window.pageYOffset;
+  let startTime =
     "now" in window.performance ? performance.now() : new Date().getTime();
 
   function scroll() {
-    var now =
+    let now =
       "now" in window.performance ? performance.now() : new Date().getTime();
-    var time = Math.min(1, (now - startTime) / duration);
-    var timeFunction = easeInOutQuad(time);
+    let time = Math.min(1, (now - startTime) / duration);
+    let timeFunction = easeInOutQuad(time);
     window.scroll(0, Math.ceil(timeFunction * (offsetBottom - start) + start));
 
     if (Math.abs(window.pageYOffset - offsetBottom) < 1 || time >= 1) {
@@ -57,4 +57,3 @@ function scrollToHistory() {
 
   scroll();
 }
-*/
