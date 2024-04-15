33abe8604d42c703733e61df346c30da34127148
danielOkpe <145658821+danielOkpe@users.noreply.github.com>
2024-04-15 12:29:52 +0200

    commentaire pour expliquer

--- site_restaurant/index.js
+++ site_restaurant/index.js
@@ -28,13 +28,13 @@
 }
 */
 
-const modal = document.getElementById("modal");
-const overlay = document.getElementById("overlay");
-const modalImg = document.getElementById("modal_img");
-const nomPlat = document.getElementById("nom_plat");
-const descriptionId = document.getElementById("description_plat");
-let dishName;
-let descriptionPlat;
+const modal = document.getElementById("modal"); // on initialise "modal" avec la div qui a pour id "modal"
+const overlay = document.getElementById("overlay"); //on initialise "overlay" avec la div qui a pour id "overlay"(c'est le fond noir qui apparait quand la modal elle s'ouvre)
+const modalImg = document.getElementById("modal_img"); //on initialise "modaleImg" avec la div qui contiendra l'image des plats
+const nomPlat = document.getElementById("nom_plat"); //on initialise "nomPlat" avec le h1 qui contiendra le nom des plats
+const descriptionId = document.getElementById("description_plat"); //on initialise "descriptionId" avec le paragraphe qui contiendra les descriptions des plats
+let dishName; // le nom des plats
+let descriptionPlat; // la description des plats
 
 function scrollToHistory() {
   let historyElement = document.getElementById("history");
@@ -66,27 +66,28 @@ function scrollToHistory() {
   scroll();
 }
 
+//fonction pour ouvrir le modal
 function openModal(index) {
-  modal.style.display = "block";
-  overlay.style.display = "block";
-  setName(index);
-  nomPlat.innerHTML = dishName;
-  modalImg.style.backgroundImage = "url('/food_img/" + index + ".jpg')";
-  setDescription(index);
-  descriptionId.innerHTML = descriptionPlat;
+  modal.style.display = "block"; //on fait apparaitre le modal
+  overlay.style.display = "block"; //on fait apparaitre le fond noir
+  setName(index); //on adapte le nom du plat selon l'index
+  nomPlat.innerHTML = dishName; //on remplace le h1 par le bon nom de plat
+  modalImg.style.backgroundImage = "url('/food_img/" + index + ".jpg')"; //on met en fond l'image du plat qui correspond
+  setDescription(index); //on adapte la description selon l'index
+  descriptionId.innerHTML = descriptionPlat; // on remplace le contenu du paragraphe par la description qui correspond
 }
 
+//fonction pour fermer le modal
 function closeModal() {
-  const modal = document.getElementById("modal");
-  const overlay = document.getElementById("overlay");
-  modal.style.display = "none";
-  overlay.style.display = "none";
+  modal.style.display = "none"; //on fait disparaitre le modal
+  overlay.style.display = "none"; // on fait disparaitre le fond noir
 }
 
+//fonction pour adapter le nom du plat selon "i" (qui sera "index" plus tard)
 function setName(i) {
   switch (i) {
     case 1:
-      dishName = "namPlat 1";
+      dishName = "namPlat 1"; // il faut remplacer  "namPlat 1" par le nom du plat de la première photo en partant de la gauche
       break;
     case 2:
       dishName = "namPlat 2";
@@ -94,7 +95,7 @@ function setName(i) {
     case 3:
       dishName = "namPlat 3";
       break;
-    case 4:
+    case 4: //on fait pareil pour tout les noms de plats
       dishName = "namPlat 4";
       break;
     case 5:
@@ -108,6 +109,7 @@ function setName(i) {
   }
 }
 
+//fonction pour adapter la description selon "i" (qui sera "index plus tard")
 function setDescription(i) {
   switch (i) {
     case 1:
@@ -119,7 +121,7 @@ function setDescription(i) {
     case 3:
       descriptionPlat = "description 3";
       break;
-    case 4:
+    case 4: //il faut remplacer "description i" par les bonnes descriptions
       descriptionPlat = "description 4";
       break;
     case 5:
